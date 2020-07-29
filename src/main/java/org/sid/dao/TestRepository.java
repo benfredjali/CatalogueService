8c83e0524b89c52fbe8216c152d5c11eddcd51f6
ali <benfredj1991ali@gmail.com>
2020-07-29 15:43:03 +0200

    changement de code

---
+++ src/main/java/org/sid/dao/TestRepository.java
@@ -0,0 +1,4 @@
+package org.sid.dao;
+
+public interface TestRepository {
+}
---
+++ src/test/java/org/sid/CatalogueServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package org.sid;
+
+
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
